69ea92bf30ed261421bb51b37d31bab818b590f4
Cre0 <yugo2801@gmail.com>
2023-01-11 23:07:33 +0800

    Design Update

--- frontend/view/loginpage.php
+++ frontend/view/loginpage.php
@@ -6,14 +6,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Loginpage</title>
+    <!-- external-css -->
+    <link rel="stylesheet" href="../css/loginpage.css">
     <!-- bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-
-    <!-- external-css -->
-    <link rel="stylesheet" href="../css/loginpage.css">
 
     <!-- ajax -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
@@ -24,41 +23,112 @@
 
 <body>
     <div class="container">
-        <div class="loginbox">
-            <div class="alert alert-danger" role="alert" id="alert" hidden>
-                A simple danger alert—check it out!
-            </div>
-            <div class="input-group mb-3">
-                <span class="input-group-text">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-people" viewBox="0 0 16 16">
-                        <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8Zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022ZM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816ZM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0Zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4Z" />
-                    </svg>
-                </span>
-                <div class="form-floating">
-                    <input type="text" name="username" id="username" class="form-control " placeholder="username">
-                    <label for="username">username</label>
+
+
+        <!-- xample -->
+        <!-- Section: Design Block -->
+        <section class="text-center text-lg-start">
+            <style>
+                .cascading-right {
+                    margin-right: -50px;
+                }
+
+                @media (max-width: 991.98px) {
+                    .cascading-right {
+                        margin-right: 0;
+                    }
+                }
+
+                #password,
+                #username {
+                    width: 400px;
+                    height: 50px;
+                }
+
+                .container {
+                    margin-top: 10%;
+                }
+
+                body {
+                    background: radial-gradient(circle at 18.7% 37.8%, rgb(250, 250, 250) 0%, rgb(225, 234, 238) 90%);
+                }
+
+            </style>
+
+            <!-- Jumbotron -->
+            <div class="container py-4">
+                <div class="row g-0 align-items-center">
+                    <div class="col-lg-6 mb-5 mb-lg-0">
+                        <div class="card cascading-right" style="
+            background: hsla(0, 0%, 100%, 0.55);
+            backdrop-filter: blur(30px);
+            ">
+                            <div class="card-body p-5 shadow-5 text-center mx-auto">
+                                <h2 class="fw-bold mb-5">Sign in</h2>
+                                <div class="alert alert-danger" role="alert" id="alert" hidden>
+
+                                </div>
+
+                                <!-- Email input -->
+                                <div class="input-group mb-3">
+                                    <span class="input-group-text">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-people" viewBox="0 0 16 16">
+                                            <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8Zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022ZM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816ZM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0Zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4Z" />
+                                        </svg>
+                                    </span>
+                                    <div class="form-floating">
+                                        <input type="text" name="username" id="username" class="form-control " placeholder="username">
+                                        <label for="username">username</label>
+                                    </div>
+                                </div>
+
+                                <!-- Password input -->
+                                <div class="input-group mb-3">
+                                    <span class="input-group-text" id="basic-addon1">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lock" viewBox="0 0 16 16">
+                                            <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2zM5 8h6a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1z" />
+                                        </svg>
+                                    </span>
+                                    <div class="form-floating">
+                                        <input type="password" name="password" id="password" class="form-control" placeholder="password" aria-describedby="basic-addon1">
+                                        <label for="password">password</label>
+                                    </div>
+                                </div>
+
+
+                                <!-- Submit button -->
+                                <button id="login" name="login" class="btn btn-primary btn-md btn-block mb-4">
+                                    Sign up
+                                </button>
+                                <a href="./registerpage.php"> <button id="register" name="register" class="btn btn-primary btn-block mb-4"> Register </button></a>
+
+                                <!-- Register buttons -->
+                                <div class="text-center">
+                                    <p>Visit Us On:</p>
+                                    <button type="button" class="btn btn-link btn-floating mx-1">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
+                                            <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
+                                        </svg>
+                                    </button>
+                                    <button type="button" class="btn btn-link btn-floating mx-1">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-twitter" viewBox="0 0 16 16">
+                                            <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z" />
+                                        </svg>
+                                    </button>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-6 mb-5 mb-lg-0">
+                        <img src="../img/5f886024e0f788ec824937e3_10tips.jpg" class="w-100 rounded-4 shadow-4" alt="" />
+                    </div>
                 </div>
             </div>
-
-            <div class="input-group mb-3">
-                <span class="input-group-text" id="basic-addon1">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lock" viewBox="0 0 16 16">
-                        <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2zM5 8h6a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1z" />
-                    </svg>
-                </span>
-                <div class="form-floating">
-                    <input type="password" name="password" id="password" class="form-control" placeholder="password" aria-describedby="basic-addon1">
-                    <label for="password">password</label>
-                </div>
-            </div>
-
-
-
-            <button id="login" name="login" class="btn btn-primary">Sign In</button>
-
-
-            <a href="./registerpage.php"> <button id="register" name="register" class="btn btn-primary"> register </button></a>
-        </div>
+            <!-- Jumbotron -->
+        </section>
+        <!-- Section: Design Block -->
 
     </div>
 
@@ -67,34 +137,43 @@
 </body>
 
 
-<script>image.png
-    var xample = "xample";
+<script>
     $(document).ready(function() {
-
         $('#login').click(() => {
             var Uname = $("#username").val();
             var Pword = $("#password").val();
-            $.ajax({
-                type: "POST",
-                url: "../../backend/controller/homecontrol.php",
-                data: {
-                    Uname: Uname,
-                    Pword: Pword,
-                },
-                success: function(response) {
-                    if (response == "Success") {
-                        alert("Success");
-                        window.location.href = "./homepage.php"
-                    } else {
-                        $("#login").attr('style', 'background-color: red');
-                        $("#alert").removeAttr('hidden');
-                        $("#alert").fadeOut(1500, () => {
-                            window.location.reload(true);
-                        });
 
+            if (Uname === "" && Pword === "") {
+                $("#alert").removeAttr('hidden');
+                document.getElementById("alert").innerHTML = "Input Username and Password Field"
+                $("#alert").fadeOut(3000, () => {
+                    window.location.reload(true);
+                });
+            } else {
+                $.ajax({
+                    type: "POST",
+                    url: "../../backend/controller/homecontrol.php",
+                    data: {
+                        Uname: Uname,
+                        Pword: Pword,
+                    },
+                    success: function(response) {
+                        if (response == "Success") {
+                            window.location.href = "./homepage.php"
+                        } else {
+                            $("#login").attr('style', 'background-color: red');
+                            $("#alert").removeAttr('hidden');
+                            document.getElementById("alert").innerHTML = "Wrong Username Or Password"
+                            $("#alert").fadeOut(3000, () => {
+                                window.location.reload(true);
+                            });
+
+                        }
                     }
-                }
-            });
+                });
+            }
+
+
 
         })
     });
